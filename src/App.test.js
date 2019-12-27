89a197b622cd18b7fe6468b127580772653fa9e0
AlphaGecko <mansour.jean-loup@protonmail.com>
2019-12-27 14:50:47 +0100

    Rajout des snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import { shallow } from 'enzyme';
-import { expect } from 'chai'; 
 import React from 'react'
 
-import App from './App'
+import { shallow } from 'enzyme';
+import { expect } from 'chai'; // Utilisation de la syntaxe chai
+import sinon from 'sinon'
+
 import GuessCount from './GuessCount';
+import Card from './Card'
+import App from './App'
+
+describe('<Card/>', () => {
+  it('Appel l\'événement "onClick" quand une carte est cliquée', () => {
+      const onClick = sinon.spy()
+      const wrapper = shallow(
+      <Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+      )
+
+      wrapper.simulate('click')
+      expect(onClick).to.have.been.calledWith(0)
+  })
+
+  it('Comparaison snapshot', () => {
+    const onClick = sinon.spy()
+    const wrapper = shallow(
+    <Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+    )
+
+    expect(wrapper).to.matchSnapshot()
+  })
+})
 
 describe('<App />', () => {
   it('Ne crash pas !', () => {
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,14 +2,19 @@ import chai from 'chai'
 import createChaiEnzyme from 'chai-enzyme';
 import createChaiJestDiff from 'chai-jest-diff'
 import sinonChai from 'sinon-chai'
+import chaiJestSnapshot from 'chai-jest-snapshot'
+import enzymeToJSON from 'enzyme-to-json/serializer'
 import dirtychai from 'dirty-chai'
 import { configure as configureEnzyme } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16'
 
 chai
     .use(dirtychai)
-    .use(createChaiJestDiff())
+    .use(chaiJestSnapshot) 
+    .use(sinonChai) 
+    .use(createChaiJestDiff()) // factory, donc ne pas oublier les parenthèses !
     .use(createChaiEnzyme())
-    .use(sinonChai) // N'est pas une factory, donc pas de parenthèses !
+
+expect.addSnapshotSerializer(enzymeToJSON)
 
 configureEnzyme({ adapter: new Adapter() })
